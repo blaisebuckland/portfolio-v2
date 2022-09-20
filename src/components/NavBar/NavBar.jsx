19cdb9a7edd252b824dca22a88536d8c0f73fb96
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-20 15:29:57 +0100

    Restyle navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,9 +7,8 @@ const NavBar = () => {
     <nav className={styles.navContainer}>
       <h2 className={styles.logo}>BB</h2>
       <div className={styles.navLinksContainer}>
-        <a href="" className={styles.navLinks}>Home</a>
-        <a href="" className={styles.navLinks}>About</a>
-        <a href="" className={styles.navLinks}>Portfolio</a>
+        <a href="#projects" className={styles.navLinks}>Portfolio</a>
+        <a href="#about" className={styles.navLinks}>About</a>
         <button className={styles.btn}>Get in touch</button>
       </div>
     </nav>
