fa313b02842d6272a3f1550c416e4778ceefa947
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-20 11:08:06 +0100

    Created navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,17 @@ import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <nav className={styles.navContainer}>
+      <h2 className={styles.logo}>BB</h2>
+      <div className={styles.navLinksContainer}>
+        <a href="" className={styles.navLinks}>Home</a>
+        <a href="" className={styles.navLinks}>About</a>
+        <a href="" className={styles.navLinks}>Portfolio</a>
+        <button className={styles.btn}>Get in touch</button>
+      </div>
+    </nav>
+    </>
   )
 }
 
