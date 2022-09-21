fe51f76be65638b2293103854f6a9f90170772e5
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-21 09:27:33 +0100

    Draft hero page

--- src/components/HeroPage/HeroPage.jsx
+++ src/components/HeroPage/HeroPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from './HeroPage.module.scss';
+import profile from '../../assets/profile.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 
 const HeroPage = () => {
   return (
-    <div>HeroPage</div>
+    <>
+    <div className={styles.heroContainer}>
+      <section className={styles.textContainer}>
+        <h2 className={styles.heading}>I'm a <br/>
+        <span className={styles.headingStylised}>Junior Software Developer</span>
+        </h2>
+        <p className={styles.intro}>Egestas dictumst enim dui ante nisl risus commodo. Pellentesque dignissim praesent purus in neque erat et. Cras donec ut orci eget neque bibendum cras. Vitae, ultricies arcu pretium duis.</p>
+        <div className={styles.scrollSection}>
+          <p>Scroll Down</p>
+         <FontAwesomeIcon icon={faAnglesDown} className={styles.scrollIcon}/>
+        </div>
+      </section>
+      <img src={profile} alt="profile" className={styles.profileImg}/>
+    </div>
+    <div className={styles.colourBox}></div>
+    </>
   )
 }
 
