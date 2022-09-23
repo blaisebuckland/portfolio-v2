db91cda59a183620a31ce5c869137b4fd4913c9f
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-23 12:23:50 +0100

    Create and style skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './Skills.module.scss';
 
+import { skillsArr } from '../../data/skillsData';
+
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+    <section className={styles.skillsContainer}>
+      <h2>Skills</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur provident facilis possimus mollitia officia? Animi veritatis quaerat repellat voluptates debitis!</p>
+      <ul className={styles.list}>
+        {skillsArr.map((skill) => <li key={skill.name} className={styles.listItem}> <span className={styles.skillsIcon}>{skill.icon}</span> {skill.name}</li> )}
+      </ul>
+    </section>
+
+    </>
   )
 }
 
