572b07ccc485f27a10406b318a9e69a2ae3bb28c
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-26 15:48:35 +0100

    Sidenav links oppen in new tab and CV icon fixed

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -6,9 +6,9 @@ import {faGithubSquare, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 const SideNav = () => {
   return (
     <section className={styles.sideNavContainer}>
-      <a href="https://github.com/blaisebuckland"><FontAwesomeIcon icon={faGithubSquare} className={styles.icon}/></a>
-      <a href="https://resume.io/r/qC1uFVV0u" className={styles.cv}>CV</a>
-      <a href="https://www.linkedin.com/in/blaise-buckland-578595131/"><FontAwesomeIcon icon={faLinkedin} className={styles.icon}/></a>
+      <a href="https://github.com/blaisebuckland" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithubSquare} className={styles.icon}/></a>
+      <a href="https://resume.io/r/qC1uFVV0u" className={styles.cv} target="_blank" rel="noreferrer">CV</a>
+      <a href="https://www.linkedin.com/in/blaise-buckland-578595131/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} className={styles.icon}/></a>
     </section>
   )
 }
