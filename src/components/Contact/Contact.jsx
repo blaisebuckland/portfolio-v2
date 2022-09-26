f9b0953978199e4ed4b6c1a325dc2df8e819adbb
Blaise Buckland <bucklandblaise@gmail.com>
2022-09-26 15:43:53 +0100

    Style contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link} from "react-scroll";
 import styles from './Contact.module.scss';
 
 const Contact= () => {
   return (
     <>
     <section className={styles.footerContainer} id="contact">
-      <h2>Let's <span className={styles.headingStylised}>work together</span></h2>
-      <a href="mailto:bucklandblaise@gmail.com">Email</a>
-      <a href="https://resume.io/r/qC1uFVV0u">CV</a>
-      <a href="https://github.com/blaisebuckland">GitHub</a>
-      <a href="https://www.linkedin.com/in/blaise-buckland-578595131/">LinkedIn</a>
-      <button>Back to top</button>
+      <h2 className={styles.heading}>Let's <span className={styles.headingStylised}>work together</span></h2>
+      <a className={styles.contactLink} href="mailto:bucklandblaise@gmail.com">Email</a>
+      <a className={styles.contactLink}  href="https://resume.io/r/qC1uFVV0u">CV</a>
+      <a className={styles.contactLink}  href="https://github.com/blaisebuckland">GitHub</a>
+      <a className={styles.contactLink}  href="https://www.linkedin.com/in/blaise-buckland-578595131/">LinkedIn</a>
+      <Link className={styles.btn} to="navBar" smooth={true} duration={500}>Back to top</Link>
     </section>
     <section className={styles.footerBar}>
       <h2 className={styles.logo}>BB</h2>
