0fc0f38ff59f95402e3b56da27d389d0245dfd33
Blaise Buckland <bucklandblaise@gmail.com>
2022-10-02 11:27:01 +0100

    Edit about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,8 +16,9 @@ const About = () => {
             <h4 className={styles.jobHeading}>Trainee Software Developer, _nology</h4>
             <p className={styles.jobSubheading}>Oct 2021 - Sept 2022</p>
             <ul className={styles.jobList}>
-              <li className={styles.jobListItem}>Completed a self-paced bootcamp course in software development alongside full-time employment. Learning leading technologies in modern software development, with a particular focus on front-end languages. </li> 
-              <li className={styles.jobListItem}>Experience in using the frameworks and tools widely used in the industry. Learning how to fix bugs myself, find answers online and when to ask a coach for advice.</li>
+              <li className={styles.jobListItem}>Completed a self-paced bootcamp course in software development. Learning leading technologies in modern software development, with a particular focus on front-end languages. </li> 
+              <li className={styles.jobListItem}>Experience in using the frameworks and tools widely used in the industry, including Agile and Scrum.</li>
+              <li>Learning how to fix bugs myself, find answers online and when to ask a peer or coach for advice.</li>
             </ul>
           </div>
 
@@ -26,9 +27,9 @@ const About = () => {
             <h4 className={styles.jobHeading}>Business Analyst, Judicial Appointment Commission</h4>
             <p className={styles.jobSubheading}>Oct 2021 - Sept 2022</p>
             <ul className={styles.jobList}>
-              <li className={styles.jobListItem}>I was the business analyst in a small digital team in a government department. Researching requirements for the newly implemented digital platform, engaging with stakeholders from across the organisation.</li>
+              <li className={styles.jobListItem}>Business analyst in a government department. Researching requirements and engaging with stakeholders from across the organisation.</li>
               <li className={styles.jobListItem}>Producing detailed specifications and explaining these to the developers and product owner.</li>
-              <li className={styles.jobListItem}>Acting as the link between the digital team and the wider organisation to enable seamless collaboration and fast feedback for the developers.</li>
+              <li className={styles.jobListItem}>Acting as the link between the digital team and the wider organisation to enable seamless collaboration.</li>
             </ul>
           </div>
 
@@ -38,9 +39,9 @@ const About = () => {
             <h4 className={styles.jobHeading}>Programme Manager, Judicial Appointment Commission</h4>
             <p className={styles.jobSubheading}>Oct 2021 - Sept 2022</p>
             <ul className={styles.jobList}>
-              <li className={styles.jobListItem}>I was responsible for the delivery of a programme of 35 recruitment campaigns per year. </li>
-              <li className={styles.jobListItem}>Allocating work to the operational teams, setting timelines for each to ensure smooth delivery and keeping the senior leadership team informed of the progress of all teams' work.</li>
-              <li className={styles.jobListItem}>Working with officials from other government departments as well as senior judges and tribunal members to plan the recruitment processes.</li>
+              <li className={styles.jobListItem}>Responsible for the delivery of a programme of 35 recruitment campaigns per year. </li>
+              <li className={styles.jobListItem}>Allocating work to operational teams, ensuring smooth delivery and keeping the senior leadership team informed of the progress of all teams' work.</li>
+              <li className={styles.jobListItem}>Working with officials from other government departments, senior judges and tribunal members to plan the recruitment processes.</li>
             </ul>
           </div>
         </section>
