30796467079d288339e57baaa9772e49d4a98b0c
Blaise Buckland <bucklandblaise@gmail.com>
2022-10-06 08:16:52 +0100

    Card responsiveness

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -6,8 +6,8 @@ const CardFront = (props) => {
   const { name, description, image, github, url, technologies } = props.project;
 
   return (
-    <div className={styles.cardFront}>
-        <img src={image} alt={name} className={styles.projectImg}/>
+    <div className={styles.cardFront} style={{backgroundImage: `url(${image})`}} >
+        {/* <img src={image} alt={name} className={styles.projectImg}/> */}
     </div>
   )
 }
