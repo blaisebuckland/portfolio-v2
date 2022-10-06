5d6c114dbc8af82c626eea29e4103cc4ff2600bb
Blaise Buckland <bucklandblaise@gmail.com>
2022-10-06 08:18:38 +0100

    Create hamburger menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState} from 'react';
 import styles from './NavBar.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const NavBar = () => {
+
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  const toggleMenuOpen = () => setIsNavOpen(!isNavOpen);
+
+  const menuStyle = isNavOpen ? styles.menuOpen : styles.menuClosed;
+
   return (
     <>
     <nav className={styles.navContainer} id="navBar">
       <h2 className={styles.logo}>BB</h2>
-      <div className={styles.navLinksContainer}>
+      <section className={styles.navLinksContainer}>
         <a href="#projects" className={styles.navLinks}>Portfolio</a>
         <a href="#about" className={styles.navLinks}>About</a>
         <a href="#contact" className={`${styles.navLinks} ${styles.contactLink}`}>Contact me</a>
-      </div>
+      </section>
+
+      <section className={styles.menuContainer}>
+          <FontAwesomeIcon icon={faBars} className={styles.menuIcon} onClick={toggleMenuOpen} />
+            <div className={menuStyle}>
+              <a href="#projects" className={styles.navLinks}>Portfolio</a>
+              <a href="#about" className={styles.navLinks}>About</a>
+              <a href="#contact" className={`${styles.navLinks}`}>Contact me</a>
+            </div> 
+      </section>
     </nav>
+
     </>
   )
 }
