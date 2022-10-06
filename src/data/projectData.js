8045b4915e37546a3ccd451a84e817eae1b56ff0
Blaise Buckland <bucklandblaise@gmail.com>
2022-10-06 08:20:55 +0100

    Fix project preview urls

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -20,7 +20,7 @@ export const projectData = [
       "This is a React app which pulls data from the Punk API. Beers are displayed on cards and can be filtered using a search box or by selecting a filter. The aim of the project was to develop my skills in React, working with APIs and unit testing.",
     image: brewdogImg,
     github: "https://github.com/blaisebuckland/brewdog",
-    url: "https://blaisebuckland.github.io/brewdog/",
+    url: "https://blaisebuckland.github.io/brewdog",
     technologies: ["React", "API Integration", "Enzyme", "SCSS"],
   },
   {
@@ -29,7 +29,7 @@ export const projectData = [
       "This translator can translate words, numbers and simple punctuation from English to Morse code and vice versa. It is my first project using JavaScript classes and unit testing.",
     image: morseImg,
     github: "https://github.com/blaisebuckland/morse-code-translator",
-    url: "blaisebuckland.github.io/morse-code-translator",
+    url: "https://blaisebuckland.github.io/morse-code-translator",
     technologies: ["JavaScript", "Jest"],
   },
   {
@@ -38,7 +38,7 @@ export const projectData = [
       "This is a battleship-like game in which the player presses a button to hit a random ship in the enemy fleet. There is a mothership, five defence ships and eight attack ships. The game ends when the player has either destroyed the mothership or has destroyed all ships. It was built using an OOP appraoch and was unit tested using Jest.",
     image: hostileAliensImg,
     github: "https://github.com/blaisebuckland/hostile-aliens",
-    url: "blaisebuckland.github.io/hostile-aliens/",
+    url: "https://blaisebuckland.github.io/hostile-aliens",
     technologies: ["JavaScript", "Jest", "OOP"],
   },
   {
@@ -47,7 +47,7 @@ export const projectData = [
       "This calculator is built in vanilla JS. The user can can perform basic mathematical operations (+,-,x,÷). The calculator can also calculate percentages and handle decimals.",
     image: calculatorImg,
     github: "https://github.com/blaisebuckland/calculator",
-    url: "blaisebuckland.github.io/calculator/",
+    url: "https://blaisebuckland.github.io/calculator",
     technologies: ["JavaScript", "SCSS", "BEM"],
   },
 ];
